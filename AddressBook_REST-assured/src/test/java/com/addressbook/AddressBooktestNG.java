ba3b3d7467a2522c4a7205f547779b9bb1697760
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-05-28 19:18:04 +0530

    Added:To Add Multiple data to the JSON file using JSON server

---
+++ AddressBook_REST-assured/src/test/java/com/addressbook/AddressBooktestNG.java
@@ -0,0 +1,47 @@
+package com.addressbook;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.*;
+
+public class AddressBooktestNG {
+	
+	@DataProvider(name = "dataforpost")
+	public Object[][] dataforPost() {
+		return new Object[][] {
+			{"Rizvaan", "Khan", "Graham Cross","New Caledonia","6701234567","rizvaan@gmail.com"},
+			{"Nia", "James","Seal Road","Las Vegas","4501345682","Njames@orkut.com"},
+			{"Robert", "Hook","Patt Hills","Indiana","8145289903","robert345@outlook.com"}
+		};	
+	}
+	
+	@Test(dataProvider = "dataforpost")
+	public void givenMultipleRecords_shouldReturn_201statusCode(String firstName, String lastName, String Address, String City,
+			                                                     String PhoneNumber, String EmailId) {
+		JSONObject request = new JSONObject();
+		
+		request.put("firstName", firstName);
+		request.put("lastName",  lastName);
+		request.put("Address", Address);
+		request.put("City", City);
+		request.put("PhoneNumber", PhoneNumber);
+		request.put("EmailId", EmailId);
+		
+		baseURI ="http://localhost";
+		port = 3000;
+		
+		given().
+		       contentType(ContentType.JSON).
+		       accept(ContentType.JSON).
+		       header("Content-Type", "application/json").
+		       body(request.toJSONString()).
+		when().
+		      post("/AddressBook").
+		then().
+		      statusCode(201).
+		      log().all();
+	}
+
+}
